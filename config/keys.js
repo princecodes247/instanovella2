296798cd047240f92b77fae4668da47a831891ab
princecodes247 <70610428+princecodes247@users.noreply.github.com>
2020-11-04 08:37:07 +0100

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+dbPassword = 'mongodb://127.0.0.1/instanovella';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+
+//+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true'
